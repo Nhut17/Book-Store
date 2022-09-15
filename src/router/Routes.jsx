23abb3480e1c1b91eac8d1a20ff4f792694dd017
Nhut <quynhnhutg6@.com>
2022-09-15 21:24:24 +0700

    Upload code

---
+++ src/router/Routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Redirect, Route, Switch } from "react-router-dom";
+
+
+const Routes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Routes
