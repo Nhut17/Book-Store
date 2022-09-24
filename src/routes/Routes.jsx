2586ec43cf768d1969b711136a8fcbfd7bdb02d9
ntdkhoaaaa <19154035@student.hcmute.edu.vn>
2022-09-24 13:37:36 +0700

    up

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import {  Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Home from '../pages/Home.jsx';
 import SignIn from '../pages/SignIn.jsx';
 import SignUp from '../pages/SignUp.jsx';
+import BestSelling from '../pages/BestSelling.jsx';
 
 const Routes = () => {
   return (
-   <Switch>
+    <Switch>
 
       {/* HOME */}
       <Route path="/" exact component={Home} />
-    
+
       {/* SIGN IN */}
       <Route path="/dang-nhap" exact component={SignIn} />
-    
+
       {/* SIGN UP */}
       <Route path="/dang-ky" exact component={SignUp} />
-    
-      
-   </Switch>
+
+      <Route path="/sach-ban-chay" exact component={BestSelling} />
+
+
+    </Switch>
   );
 };
 
