6c096d21acec68f86ba8af2de6ecc1b691d91843
Nhut <quynhnhutg6@.com>
2022-11-09 16:42:37 +0700

    up cart 50%

---
+++ .history/src/components/Layout_20221109162007.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+import { BrowserRouter } from "react-router-dom";
+import Router from "../routes/Router";
+import Footer from "./Footer";
+import Header from "./Header";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { LoginContext } from '../context/loginContext'
+import 'react-responsive-modal/styles.css';
+import { useSelector } from "react-redux";
+
+
+const Layout = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const [layoutModal, setLayoutModal] = useState()
+  const [showSignUp, setShowSignUp] = useState(false)
+  const user = useSelector(state => state.user)
+
+  useEffect(() => {
+    if(user.success)
+    {
+      toast(`Chào mừng ${user.user.name}`, 
+      {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+  },[user.success])
+  return (
+
+    <BrowserRouter>
+      {
+          user.success &&  
+          <ToastContainer />
+      }
+          <div className="layout">
+            {
+              showLogin && <Login setShowLogin={setShowLogin} />
+            }
+            {
+              showSignUp && <SignUp setShowSignUp={setShowSignUp} />
+            }
+
+            <LoginContext.Provider 
+                        value={[showLogin, setShowLogin, setLayoutModal, setShowSignUp]}>
+              <Header />
+              <Router />
+            </LoginContext.Provider>
+
+
+            <Footer />
+          </div>
+      
+      
+      
+    </BrowserRouter>
+
+
+  );
+};
+
+export default Layout;
---
+++ .history/src/redux/reducer/cartSlice_20221109143010.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+const initialState = {
+    cart: 'haha',
+    loading: true,
+    success: false,
+}
+
+const cartAPI = 'http://localhost:8083/user/cart'
+
+export const addCart = createAsyncThunk('cart/addCart', 
+        async(data,thunkAPI) => {
+            try{
+                const token = localStorage.getItem('token');
+                console.log('token: ' + token );
+                const res = await axios.post('http://localhost:8083/user/cart/add',data,{
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                return res.data
+            }
+            catch(e)
+            {
+                return thunkAPI.rejectWithValue('Error with api add cart')
+            }
+        })
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    extraReducers: {
+        [addCart.fulfilled]: (state,action) => {
+            
+        },
+        [addCart.rejected]: (state,action) =>{
+
+        }
+    }
+})
+
+export default cartSlice.reducer
